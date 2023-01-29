13fefbe161b4e306f9f7fd9c2e704a8c0bf7d18e
manoaiharivola <manoaiharivola@gmail.com>
2023-01-29 22:18:20 +0300

    feat : dynamic sidebar

---
+++ src/app/layout/components/utilisateur/utilisateur-template/sidebar-data.ts
@@ -0,0 +1,7 @@
+export const sidebarData = [
+  {
+    routerLink: '/garage/accueil',
+    icon: 'mdi mdi-car',
+    label: 'Voitures'
+  }
+];
--- src/app/layout/components/utilisateur/utilisateur-template/utilisateur-template.component.ts
+++ src/app/layout/components/utilisateur/utilisateur-template/utilisateur-template.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {sidebarData} from "./sidebar-data";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-utilisateur-template',
@@ -11,12 +13,17 @@ import { Component, OnInit } from '@angular/core';
     "../../../../backoffice/vendors/datatables.net-bs4/dataTables.bootstrap4.css",
     "../../../../backoffice/vendors/ti-icons/css/themify-icons.css",
     "../../../../backoffice/js/select.dataTables.min.css",
-    "../../../../backoffice/css/vertical-layout-light/style.css"
+    "../../../../backoffice/css/vertical-layout-light/style.css",
+    "../../../../backoffice/vendors/mdi/css/materialdesignicons.min.css"
   ]
 })
 export class UtilisateurTemplateComponent implements OnInit {
+  sidebarData = sidebarData;
+  currentPath: string;
 
-  constructor() { }
+  constructor(private location: Location) {
+    this.currentPath = this.location.path();
+  }
 
   ngOnInit(): void {
   }
