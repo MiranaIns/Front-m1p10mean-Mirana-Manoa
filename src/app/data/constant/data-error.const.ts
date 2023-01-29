264eff5bf9a9c385dfeaecd325e9ade99686ecfe
manoaiharivola <manoaiharivola@gmail.com>
2023-01-29 23:48:17 +0300

    feat : call voitures service

---
+++ src/app/data/constant/data-error.const.ts
@@ -0,0 +1,11 @@
+export const DataWsConst = {
+  // authService
+  WS_LOGIN : '/authentification/login',
+  WS_INSCRIPTION : '/authentification/register',
+
+  //voituresService
+  WS_GET_ALL_VOITURES : "/voitures",
+
+  // adminAuthService
+  WS_ADMIN_LOGIN : '/admin/authentification/login'
+}
