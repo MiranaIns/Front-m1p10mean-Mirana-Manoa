f9689a888ae07482888d9ebc07cef54c34eef477
manoaiharivola <manoaiharivola@gmail.com>
2023-01-30 17:24:37 +0300

    feat : pop up css

--- src/app/shared/components/faire-devis-pop-up/faire-devis-pop-up.component.ts
+++ src/app/shared/components/faire-devis-pop-up/faire-devis-pop-up.component.ts
@@ -4,9 +4,14 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 @Component({
   selector: 'app-faire-devis-pop-up',
   templateUrl: './faire-devis-pop-up.component.html',
-  styleUrls: ['./faire-devis-pop-up.component.css']
+  styleUrls: ['./faire-devis-pop-up.component.css',
+    "../../../template/vendors/feather/feather.css",
+    "../../../template/vendors/ti-icons/css/themify-icons.css",
+    "../../../template/vendors/css/vendor.bundle.base.css",
+    "../../../template/css/vertical-layout-light/style.css"]
 })
 export class FaireDevisPopUpComponent implements OnInit {
+  voiture_detail: any;
 
   constructor(
     public dialogRef: MatDialogRef<FaireDevisPopUpComponent>,
@@ -14,6 +19,7 @@ export class FaireDevisPopUpComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.data.voiture_garage);
+    this.voiture_detail = this.data.voiture_garage.voiture_details;
   }
 
   onNoClick(): void {
